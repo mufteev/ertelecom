3a0ad15f393198d829c7a44e22fb7faad415efd7
ippolit <pandorabox2015@outlook.com>
2021-07-17 23:43:19 +0500

    Rename to Async

--- client/src/util/request.js
+++ client/src/util/request.js
@@ -1,13 +1,25 @@
-export function getJSON(url) {
-  return fetch(url, { method: 'GET' })
-    .then(response => response.ok ? response.json() : Promise.reject(response));
+async function request(url, option) {
+  try {
+    const response = await fetch(url, option);
+    if (!response.ok) throw response;
+    const json = await response.json();
+
+    if (json.errorCode === 0) {
+      return json.data;
+    } else {
+      throw TypeError(json.errorMessage);
+    }
+  } catch (e) {
+    const error = e instanceof Response
+      ? `${ e.statusText } [${ e.url }]`
+      : e instanceof TypeError ? e.message : e;
+    await Promise.reject(error);
+  }
 }
 
-export function postJSON(url, body) {
-  return fetch(url, {
-    method : 'POST',
-    body   : JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' }
-  })
-    .then(response => response.ok ? response.json() : Promise.reject(response));
-}
+export const getJsonAsync = async (url) => request(url, { method: 'GET' });
+export const postJsonAsync = async (url, body) => request(url, {
+  method : 'POST',
+  body   : JSON.stringify(body),
+  headers: { 'Content-Type': 'application/json' }
+});
